f17a33e88f079794243cae51da819cce71e0fced
TDevD <dev@samouraiwallet.com>
2018-10-28 11:27:25 +0100

    APIFactory: UTXO get/parse

--- app/src/main/java/com/samourai/sentinel/api/APIFactory.java
+++ app/src/main/java/com/samourai/sentinel/api/APIFactory.java
@@ -13,6 +13,8 @@ import com.samourai.sentinel.sweep.UTXO;
 import com.samourai.sentinel.util.AddressFactory;
 import com.samourai.sentinel.util.Web;
 
+import org.apache.commons.lang3.StringUtils;
+import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.script.Script;
 import org.json.JSONArray;
@@ -20,7 +22,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.bouncycastle.util.encoders.Hex;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -32,8 +36,11 @@ public class APIFactory	{
     private static long xpub_balance = 0L;
     private static HashMap<String, Long> xpub_amounts = null;
     private static HashMap<String,List<Tx>> xpub_txs = null;
-
-    private static boolean hasShuffled = false;
+    private static HashMap<String,Integer> unspentAccounts = null;
+    private static HashMap<String,Integer> unspentBIP49 = null;
+    private static HashMap<String,Integer> unspentBIP84 = null;
+    private static HashMap<String,String> unspentPaths = null;
+    private static HashMap<String,UTXO> utxos = null;
 
     private static APIFactory instance = null;
     private static Context context = null;
@@ -48,7 +55,11 @@ public class APIFactory	{
             xpub_amounts = new HashMap<String, Long>();
             xpub_txs = new HashMap<String,List<Tx>>();
             xpub_balance = 0L;
-            hasShuffled = false;
+            unspentPaths = new HashMap<String, String>();
+            unspentAccounts = new HashMap<String, Integer>();
+            unspentBIP49 = new HashMap<String, Integer>();
+            unspentBIP84 = new HashMap<String, Integer>();
+            utxos = new HashMap<String, UTXO>();
             instance = new APIFactory();
         }
 
@@ -122,9 +133,6 @@ public class APIFactory	{
                 JSONObject addrObj = null;
                 for(int i = 0; i < addressesArray.length(); i++)  {
                     addrObj = (JSONObject)addressesArray.get(i);
-                    if(i == 1 && addrObj.has("n_tx") && addrObj.getInt("n_tx") > 0)  {
-                        hasShuffled = true;
-                    }
                     if(addrObj.has("final_balance") && addrObj.has("address"))  {
                         xpub_amounts.put((String)addrObj.get("address"), addrObj.getLong("final_balance"));
                         if(((String)addrObj.get("address")).startsWith("xpub") || ((String)addrObj.get("address")).startsWith("ypub") || ((String)addrObj.get("address")).startsWith("zpub") || ((String)addrObj.get("address")).startsWith("tpub") || ((String)addrObj.get("address")).startsWith("upub") || ((String)addrObj.get("address")).startsWith("vpub"))    {
@@ -278,6 +286,122 @@ public class APIFactory	{
         Collections.sort(ret, new TxMostRecentDateComparator());
 
         return ret;
+    }
+
+    public synchronized JSONObject getUnspentOutputs(String[] xpubs) {
+
+        String _url = SamouraiSentinel.getInstance().isTestNet() ? Web.SAMOURAI_API2_TESTNET : Web.SAMOURAI_API2;
+
+        JSONObject jsonObject  = null;
+
+        try {
+
+            String response = null;
+
+            StringBuilder args = new StringBuilder();
+            args.append("active=");
+            args.append(StringUtils.join(xpubs, URLEncoder.encode("|", "UTF-8")));
+            Log.d("APIFactory", "UTXO args:" + args.toString());
+            response = Web.postURL(_url + "unspent?", args.toString());
+            Log.d("APIFactory", "UTXO:" + response);
+
+            parseUnspentOutputs(response);
+
+        }
+        catch(Exception e) {
+            jsonObject = null;
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+
+    private synchronized boolean parseUnspentOutputs(String unspents)   {
+
+        if(unspents != null)    {
+
+            try {
+                JSONObject jsonObj = new JSONObject(unspents);
+
+                if(jsonObj == null || !jsonObj.has("unspent_outputs"))    {
+                    return false;
+                }
+                JSONArray utxoArray = jsonObj.getJSONArray("unspent_outputs");
+                if(utxoArray == null || utxoArray.length() == 0) {
+                    return false;
+                }
+
+                for (int i = 0; i < utxoArray.length(); i++) {
+
+                    JSONObject outDict = utxoArray.getJSONObject(i);
+
+                    byte[] hashBytes = Hex.decode((String)outDict.get("tx_hash"));
+                    Sha256Hash txHash = Sha256Hash.wrap(hashBytes);
+                    int txOutputN = ((Number)outDict.get("tx_output_n")).intValue();
+                    BigInteger value = BigInteger.valueOf(((Number)outDict.get("value")).longValue());
+                    String script = (String)outDict.get("script");
+                    byte[] scriptBytes = Hex.decode(script);
+                    int confirmations = ((Number)outDict.get("confirmations")).intValue();
+
+                    try {
+                        String address = null;
+                        if(Bech32Util.getInstance().isBech32Script(script))    {
+                            address = Bech32Util.getInstance().getAddressFromScript(script);
+                        }
+                        else    {
+                            address = new Script(scriptBytes).getToAddress(SamouraiSentinel.getInstance().getCurrentNetworkParams()).toString();
+                        }
+
+                        if(outDict.has("xpub"))    {
+                            JSONObject xpubObj = (JSONObject)outDict.get("xpub");
+                            String path = (String)xpubObj.get("path");
+                            String m = (String)xpubObj.get("m");
+                            unspentPaths.put(address, path);
+                            if(SamouraiSentinel.getInstance().getBIP49().containsKey(m))    {
+                                unspentBIP49.put(address, 0);   // assume account 0
+                            }
+                            else if(SamouraiSentinel.getInstance().getBIP84().containsKey(m))    {
+                                unspentBIP84.put(address, 0);   // assume account 0
+                            }
+                            else    {
+                                unspentAccounts.put(address, AddressFactory.getInstance(context).xpub2account().get(m));
+                            }
+                        }
+                        else    {
+                            ;
+                        }
+
+                        // Construct the output
+                        MyTransactionOutPoint outPoint = new MyTransactionOutPoint(txHash, txOutputN, value, scriptBytes, address);
+                        outPoint.setConfirmations(confirmations);
+
+                        if(utxos.containsKey(script))    {
+                            utxos.get(script).getOutpoints().add(outPoint);
+                        }
+                        else    {
+                            UTXO utxo = new UTXO();
+                            utxo.getOutpoints().add(outPoint);
+                            utxos.put(script, utxo);
+                        }
+
+                    }
+                    catch(Exception e) {
+                        ;
+                    }
+
+                }
+
+                return true;
+
+            }
+            catch(JSONException je) {
+                ;
+            }
+
+        }
+
+        return false;
+
     }
 
     public synchronized UTXO getUnspentOutputsForSweep(String address) {
