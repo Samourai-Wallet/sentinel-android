44abcfcf119a86b3471b7f88938702fabf4514a8
Sarath <sarathkumars677@gmail.com>
2020-08-18 21:34:04 +0530

    kotlin-coroutines-okhttp

---
+++ app/src/main/java/com/samourai/sentinel/api/okHttp/CallStackRecorder.kt
@@ -0,0 +1,29 @@
+@file:Suppress("RedundantVisibilityModifier")
+package com.samourai.sentinel.api.okHttp
+
+/**
+ * sentinel-android
+ *
+ * @author Sarath
+ */
+
+
+
+public const val OKHTTP_STACK_RECORDER_PROPERTY = "ru.gildor.coroutines.okhttp.stackrecorder"
+
+/**
+ * Debug turned on value for [DEBUG_PROPERTY_NAME]. See [newCoroutineContext][CoroutineScope.newCoroutineContext].
+ */
+public const val OKHTTP_STACK_RECORDER_ON = "on"
+
+/**
+ * Debug turned on value for [DEBUG_PROPERTY_NAME]. See [newCoroutineContext][CoroutineScope.newCoroutineContext].
+ */
+public const val OKHTTP_STACK_RECORDER_OFF = "off"
+
+@JvmField
+val isRecordStack = when (System.getProperty(OKHTTP_STACK_RECORDER_PROPERTY)) {
+    OKHTTP_STACK_RECORDER_ON -> true
+    OKHTTP_STACK_RECORDER_OFF, null, "" -> false
+    else -> error("System property '$OKHTTP_STACK_RECORDER_PROPERTY' has unrecognized value '${System.getProperty(OKHTTP_STACK_RECORDER_PROPERTY)}'")
+}
