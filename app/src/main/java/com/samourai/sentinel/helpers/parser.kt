bc2d4d74de4bd4f6db760f75e0fa57fc8f40c06f
Sarath <sarathkumars677@gmail.com>
2020-08-27 11:31:05 +0530

    Helper functions for parsing with gson

--- app/src/main/java/com/samourai/sentinel/helpers/parser.kt
+++ app/src/main/java/com/samourai/sentinel/helpers/parser.kt
@@ -1,6 +1,27 @@
 package com.samourai.sentinel.helpers
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.google.gson.Gson
+import java.lang.Exception
+import java.lang.reflect.Type
 
-val mapper = ObjectMapper().registerModule(KotlinModule())
+val gson: Gson = Gson()
+
+fun Any.toJSON(): String? {
+    return try {
+        Gson().toJson(this)
+    } catch (Ex: Exception) {
+        null
+    }
+}
+
+inline fun <reified T> fromJSON(payload: String, type: Type? = null): T? {
+    return try {
+        return if(type!=null){
+            gson.fromJson<T>(payload, type)
+        }else{
+            gson.fromJson<T>(payload, T::class.java)
+        }
+    } catch (Ex: Exception) {
+        null
+    }
+}
