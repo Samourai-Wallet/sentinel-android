49789a0565d2595093bbf963dc01babd4175d919
Sarath <sarathkumars677@gmail.com>
2020-08-27 11:13:46 +0530

    Api config files

--- app/src/main/java/com/samourai/sentinel/api/APIConfig.kt
+++ app/src/main/java/com/samourai/sentinel/api/APIConfig.kt
@@ -8,11 +8,11 @@ package com.samourai.sentinel.api
 
 object APIConfig  {
 
-    val SAMOURAI_API = "https://api.samouraiwallet.com/v2/"
-    val SAMOURAI_API_TESTNET = "https://api.samouraiwallet.com/test/v2/"
+    val SAMOURAI_API = "https://api.samouraiwallet.com/v2"
+    val SAMOURAI_API_TESTNET = "https://api.samouraiwallet.com/test/v2"
 
 
-    val SAMOURAI_API_TOR = "http://d2oagweysnavqgcfsfawqwql2rwxend7xxpriq676lzsmtfwbt75qbqd.onion/v2/"
-    val SAMOURAI_API_TOR_TESTNET = "http://d2oagweysnavqgcfsfawqwql2rwxend7xxpriq676lzsmtfwbt75qbqd.onion/test/v2/"
+    val SAMOURAI_API_TOR = "http://d2oagweysnavqgcfsfawqwql2rwxend7xxpriq676lzsmtfwbt75qbqd.onion/v2"
+    val SAMOURAI_API_TOR_TESTNET = "http://d2oagweysnavqgcfsfawqwql2rwxend7xxpriq676lzsmtfwbt75qbqd.onion/test/v2"
 
 }
