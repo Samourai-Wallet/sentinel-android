eb2c27164c73f44461f83d054316c05e5fbee261
Sarath <sarathkumars677@gmail.com>
2020-08-31 21:01:45 +0530

    Home Screen clean ups

--- app/src/main/java/com/samourai/sentinel/ui/home/HomeActivity.kt
+++ app/src/main/java/com/samourai/sentinel/ui/home/HomeActivity.kt
@@ -1,53 +1,223 @@
 package com.samourai.sentinel.ui.home
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.util.Log
+import android.view.*
 import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.samourai.sentinel.BuildConfig
 import com.samourai.sentinel.R
+import com.samourai.sentinel.api.APIConfig
+import com.samourai.sentinel.core.SentinelState
+import com.samourai.sentinel.tor.TorServiceController
+import com.samourai.sentinel.service.WebSocketHandler
+import com.samourai.sentinel.service.WebSocketService
 import com.samourai.sentinel.ui.SentinelActivity
 import com.samourai.sentinel.ui.adapters.CollectionsAdapter
-import com.samourai.sentinel.ui.collectionDetails.CollectionDetails
-import com.samourai.sentinel.ui.dojo.NetworkActivity
+import com.samourai.sentinel.ui.collectionDetails.CollectionDetailsActivity
+import com.samourai.sentinel.ui.dojo.DojoConfigureBottomSheet
+import com.samourai.sentinel.ui.settings.NetworkActivity
 import com.samourai.sentinel.ui.fragments.AddNewPubKeyBottomSheet
-import com.samourai.sentinel.ui.utils.RecyclerViewItemDividerDecorator
+import com.samourai.sentinel.ui.settings.SettingsActivity
+import com.samourai.sentinel.ui.utils.*
+import com.samourai.sentinel.ui.views.confirm
+import io.matthewnelson.topl_service.prefs.TorServicePrefs
 import kotlinx.android.synthetic.main.activity_home.*
+import org.koin.java.KoinJavaComponent.inject
+import timber.log.Timber
+import java.lang.Exception
 
 
 class HomeActivity : SentinelActivity() {
 
     private lateinit var linearLayoutManager: LinearLayoutManager
     private val collectionsAdapter = CollectionsAdapter();
-
+    private val webSocketHandler: WebSocketHandler by inject(WebSocketHandler::class.java);
+    private val prefsUtil: PrefsUtil by inject(PrefsUtil::class.java);
+    private var connectingDojo = false
+    private lateinit var torServicePrefs: TorServicePrefs
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        setSupportActionBar(findViewById(R.id.toolbarCollectionDetails))
+        setSupportActionBar(toolbarHome)
+        torServicePrefs = TorServicePrefs(this)
 
         val model: HomeViewModel by viewModels()
 
+        setUp()
+
         setUpCollectionList()
 
-        model.collections.observe(this, Observer { collectionsAdapter.update(it) })
-
+        model.getCollections().observe(this, {
+            collectionsAdapter.update(it)
+        })
+        model.getBalance().observe(this, { updateBalance(it) })
         fab.setOnClickListener {
-            val bottomSheetFragment = AddNewPubKeyBottomSheet()
-            bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+            if (prefsUtil.haptics!!) {
+                fab.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK)
+            }
+            connectingDojo = false
+            if (!AndroidUtil.isPermissionGranted(Manifest.permission.CAMERA, applicationContext)) {
+                this.askCameraPermission()
+            } else {
+                showPubKeyBottomSheet()
+            }
+
         }
 
+        model.loading().observe(this, {
+            swipeRefreshCollection.isRefreshing = it
+        })
+        model.getErrorMessage().observe(this, {
+            if (it != "null")
+                this@HomeActivity.showFloatingSnackBar(fab, text = "Error: $it")
+        })
+
+        swipeRefreshCollection.setOnRefreshListener {
+            swipeRefreshCollection.isRefreshing = false
+            if (SentinelState.isTorRequired()) {
+                if (SentinelState.torState == SentinelState.TorState.WAITING) {
+                    this.showFloatingSnackBar(fab,
+                            text = "Tor is bootstrapping! please wait and try again")
+                }
+                if (SentinelState.torState == SentinelState.TorState.OFF) {
+                    this.showFloatingSnackBar(fab,
+                            "Tor is required! Please turn on Tor",
+                            actionText = "Turn on",
+                            actionClick = {
+                                TorServiceController.startTor()
+                            })
+                }
+                if (SentinelState.torState == SentinelState.TorState.ON) {
+                    model.fetchBalance()
+                }
+            } else {
+                model.fetchBalance()
+            }
+        }
+
+//        fetch(model)
+
+
+        if (SentinelState.isTorRequired()) {
+            SentinelState.torStateLiveData().observe(this, {
+                if (it == SentinelState.TorState.ON)
+                    WebSocketService.start(applicationContext)
+            })
+        } else {
+            WebSocketService.start(applicationContext)
+        }
+
+    }
+
+
+    private fun fetch(model: HomeViewModel) {
+        val lastSync = prefsUtil.lastSynced!!
+        val currentTime = System.currentTimeMillis()
+        if (currentTime.minus(lastSync) >= 200000) {
+            if (SentinelState.isTorRequired() && SentinelState.isTorStarted()) {
+                model.fetchBalance()
+            } else {
+                SentinelState.torStateLiveData().observe(this, Observer {
+                    if (it == SentinelState.TorState.ON) {
+                        model.fetchBalance()
+                    }
+                })
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (SentinelState.isTestNet() && !title.contains("TestNet")) {
+            title = "$title | TestNet"
+        }
+    }
+
+    private fun setUp() {
+        if (prefsUtil.firstRun!! && BuildConfig.DEBUG) {
+            this.confirm(label = "Choose network",
+                    positiveText = "TestNet",
+                    negativeText = "MainNet",
+                    onConfirm = { confirm ->
+
+                        prefsUtil.firstRun = false
+                        if (confirm) {
+                            prefsUtil.testnet = true
+                        }
+                        if (!SentinelState.isTestNet()) {
+                            title = "$title".removeSuffix("| TestNet")
+                        }
+                        showServerConfig()
+                    })
+        } else {
+            showServerConfig()
+        }
+    }
+
+    private fun showServerConfig() {
+        if (prefsUtil.apiEndPoint.isNullOrEmpty()) {
+            this.confirm(label = "Notice",
+                    message = getString(R.string.server_config_instruction),
+                    positiveText = "Connect to Dojo",
+                    negativeText = "Use default server",
+                    isCancelable = false,
+                    onConfirm = { confirm ->
+                        if (confirm) {
+                            connectingDojo = true
+                            if (!AndroidUtil.isPermissionGranted(Manifest.permission.CAMERA, applicationContext)) {
+                                this.askCameraPermission()
+                            } else {
+                                showDojoSetUpBottomSheet()
+                            }
+                        } else {
+                            if (prefsUtil.testnet!!) {
+                                prefsUtil.apiEndPoint = APIConfig.SAMOURAI_API_TESTNET
+                                prefsUtil.apiEndPointTor = APIConfig.SAMOURAI_API_TOR_TESTNET
+                            } else {
+                                prefsUtil.apiEndPoint = APIConfig.SAMOURAI_API
+                                prefsUtil.apiEndPointTor = APIConfig.SAMOURAI_API_TOR
+                            }
+                        }
+                    })
+
+        }
+    }
+
+    private fun showDojoSetUpBottomSheet() {
+        val dojoConfigureBottomSheet = DojoConfigureBottomSheet()
+        dojoConfigureBottomSheet.show(supportFragmentManager, dojoConfigureBottomSheet.tag)
+        dojoConfigureBottomSheet.setDojoConfigurationListener(object : DojoConfigureBottomSheet.DojoConfigurationListener {
+            override fun onDismiss() {
+                if (!prefsUtil.isAPIEndpointEnabled()) {
+                    showServerConfig()
+                }
+            }
+        })
+    }
+
+    private fun updateBalance(it: String?) {
+        homeBalanceBtc.text = it
+    }
+
+    private fun showPubKeyBottomSheet() {
+        val bottomSheetFragment = AddNewPubKeyBottomSheet()
+        bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
     }
 
 
     private fun setUpCollectionList() {
 
         collectionsAdapter.setOnClickListener {
-            startActivity(Intent(applicationContext, CollectionDetails::class.java).apply {
+            startActivity(Intent(applicationContext, CollectionDetailsActivity::class.java).apply {
                 putExtra("collection", it.id)
             })
         }
@@ -58,15 +228,56 @@ class HomeActivity : SentinelActivity() {
         collectionRecyclerView.apply {
             adapter = collectionsAdapter
             layoutManager = linearLayoutManager
+            itemAnimator = SlideInItemAnimator(slideFromEdge = Gravity.TOP)
             setHasFixedSize(true)
             addItemDecoration(decorator)
         }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == CAMERA_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (connectingDojo) {
+                showDojoSetUpBottomSheet()
+                return
+            }
+            showPubKeyBottomSheet()
+
+        } else {
+            if (requestCode == CAMERA_PERMISSION && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
+                if (connectingDojo) {
+                    showDojoSetUpBottomSheet()
+                    return
+                }
+                showPubKeyBottomSheet()
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home_options_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.home_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onBackPressed() {
+        MaterialAlertDialogBuilder(this)
+                .setTitle(resources.getString(R.string.confirm_exit))
+                .setMessage(resources.getString(R.string.ask_you_sure_exit))
+                .setNegativeButton(resources.getString(R.string.no)) { _, _ ->
+                }
+                .setPositiveButton(resources.getString(R.string.yes)) { _, _ ->
+                    super.onBackPressed()
+                }
+                .show()
     }
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
@@ -74,19 +285,38 @@ class HomeActivity : SentinelActivity() {
         return super.onPrepareOptionsMenu(menu)
     }
 
-
     private fun setNetWorkMenu(menu: Menu) {
         val alertMenuItem: MenuItem = menu.findItem(R.id.activity_home_menu_network)
         val rootView = alertMenuItem.actionView
-        val redCircle = rootView.findViewById<View>(R.id.home_menu_network_shape) as FrameLayout
+        val statusCircle = rootView.findViewById<View>(R.id.home_menu_network_shape) as FrameLayout
         val shape = ContextCompat.getDrawable(applicationContext, R.drawable.circle_shape);
         shape?.setTint(ContextCompat.getColor(applicationContext, R.color.red))
-        redCircle.background = shape
-        redCircle.visibility = View.VISIBLE
+        statusCircle.background = shape
+        statusCircle.visibility = View.VISIBLE
+        SentinelState.torStateLiveData().observe(this, Observer {
+            if (it == SentinelState.TorState.ON) {
+                shape?.setTint(ContextCompat.getColor(applicationContext, R.color.green_ui_2))
+            }
+            if (it == SentinelState.TorState.OFF) {
+                shape?.setTint(ContextCompat.getColor(applicationContext, R.color.red))
+            }
+            if (it == SentinelState.TorState.WAITING) {
+                shape?.setTint(ContextCompat.getColor(applicationContext, R.color.warning_yellow))
+            }
+            statusCircle.background = shape
+            statusCircle.visibility = View.VISIBLE
+        })
+
         rootView.setOnClickListener {
             startActivity(Intent(this, NetworkActivity::class.java))
         }
     }
 
-}
+    fun connectSocket() {
+        try {
+            webSocketHandler.connect()
+        } catch (ex: Exception) {
+        }
+    }
 
+}
--- app/src/main/java/com/samourai/sentinel/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/samourai/sentinel/ui/home/HomeViewModel.kt
@@ -1,46 +1,125 @@
 package com.samourai.sentinel.ui.home
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.samourai.sentinel.data.CollectionModel
+import androidx.lifecycle.*
+import com.samourai.sentinel.api.ApiService
+import com.samourai.sentinel.data.PubKeyCollection
 import com.samourai.sentinel.data.repository.CollectionRepository
-import kotlinx.coroutines.Dispatchers
+import com.samourai.sentinel.data.repository.TransactionsRepository
+import com.samourai.sentinel.ui.dojo.DojoUtility
+import com.samourai.sentinel.ui.utils.PrefsUtil
+import com.samourai.sentinel.util.MonetaryUtil
+import com.samourai.sentinel.util.apiScope
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.java.KoinJavaComponent.inject
-import java.sql.Time
+import timber.log.Timber
 
 class HomeViewModel : ViewModel() {
 
 
     private val repository: CollectionRepository by inject(CollectionRepository::class.java)
+    private val monetaryUtil: MonetaryUtil by inject(MonetaryUtil::class.java)
+    private val transactionsRepository: TransactionsRepository by inject(TransactionsRepository::class.java)
+    private val dojoUtility: DojoUtility by inject(DojoUtility::class.java)
+    private val apiService: ApiService by inject(ApiService::class.java)
 
-    var collections: MutableLiveData<ArrayList<CollectionModel>> = MutableLiveData()
-    private val errorMessage: MutableLiveData<String> = MutableLiveData();
+    private val errorMessage: MutableLiveData<String> = MutableLiveData()
+    private val balance: MutableLiveData<String> = MutableLiveData()
+    private var netWorkJobs: ArrayList<Job?> = arrayListOf()
+    private val prefsUtil: PrefsUtil by inject(PrefsUtil::class.java);
 
 
     init {
+        load()
+    }
+
+    private fun load() {
         viewModelScope.launch {
             try {
-                collections = repository.collectionsLiveData;
                 repository.read()
             } catch (e: Exception) {
-                e.printStackTrace()
+                Timber.e(e)
                 errorMessage.postValue(e.message);
             }
 
         }
     }
 
-    fun addNewCollection() {
-        val stamp = System.currentTimeMillis();
-        repository.addNew(CollectionModel(collectionLabel = "HELLO ${stamp}", pubs = arrayListOf(), balance = stamp))
+    fun getCollections(): LiveData<ArrayList<PubKeyCollection>> {
+        return repository.collectionsLiveData.map {
+            updateBalance()
+            it
+        }
     }
 
-    fun setVal(list: ArrayList<CollectionModel>?) {
-        collections.postValue(list);
+    fun getBalance(): LiveData<String> {
+        return balance
     }
 
+    fun fetchBalance() {
+        if (netWorkJobs.isNotEmpty()) {
+            netWorkJobs.forEach {
+                it?.cancel()
+            }
+        }
+        try {
+            repository.pubKeyCollections.forEach {
+
+                val job = apiScope.launch {
+                    try {
+                        transactionsRepository.fetchFromServer(it)
+                    } catch (e: Exception) {
+                        Timber.e(e)
+                        throw  CancellationException(e.message)
+                    }
+                }
+                netWorkJobs.add(job)
+            }
+            if (netWorkJobs.isNotEmpty()) {
+                //Save last sync time to prefs
+                netWorkJobs[netWorkJobs.lastIndex]?.let {
+                    it.invokeOnCompletion { error ->
+                        if (error != null) {
+                            Timber.e(error)
+                            errorMessage.postValue("${error.message}")
+                            return@invokeOnCompletion
+                        }
+                        prefsUtil.lastSynced = System.currentTimeMillis()
+                    }
+                }
+            }
+        } catch (ex: Exception) {
+            Timber.e(ex)
+            errorMessage.postValue("${ex.message}")
+        }
+    }
+
+    fun getErrorMessage(): LiveData<String> {
+        return errorMessage
+    }
+
+    fun loading(): LiveData<Boolean> {
+        return transactionsRepository.loadingState()
+    }
+
+    private fun updateBalance() {
+        if (repository.collectionsLiveData.value == null) {
+            return
+        }
+        if (repository.collectionsLiveData.value!!.size == 0) {
+            balance.postValue("")
+        }
+        repository.collectionsLiveData.value?.let { pubkeys ->
+            if (pubkeys.isEmpty()) {
+                return
+            }
+            try {
+                val total = pubkeys.map { it.balance }.reduce { acc, l -> acc + l }
+                balance.postValue("${monetaryUtil.formatToBtc(total)} BTC")
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
+    }
 }
