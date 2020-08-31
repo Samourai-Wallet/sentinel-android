fd690552c5f2868e389031256b3754deda543fbe
Sarath <sarathkumars677@gmail.com>
2020-08-31 17:01:27 +0530

    Updated DbHandler helper class

--- app/src/main/java/com/samourai/sentinel/data/db/DbHandler.kt
+++ app/src/main/java/com/samourai/sentinel/data/db/DbHandler.kt
@@ -1,28 +1,31 @@
 package com.samourai.sentinel.data.db
 
 import android.content.Context
-import android.util.Log
 import io.paperdb.Book
 import io.paperdb.Paper
 import org.koin.java.KoinJavaComponent.inject
 
 
-class DbHandler() {
+class DbHandler {
 
-    private val context: Context by inject(Context::class.java);
+    private val context: Context by inject(Context::class.java)
 
     private val storeLocation = "${this.context.filesDir.path}/app_wallet"
 
-
-    fun getTxStore(collectionId:String): Book {
-        return Paper.bookOn(storeLocation, "${collectionId}.txs")!!
+    fun getTxStore(): Book {
+        return Paper.bookOn(storeLocation, "sentinel.txs")!!
     }
 
-    fun getUTXOsStore(): Book? {
+    fun getUTXOsStore(): Book {
         return Paper.bookOn(storeLocation, "sentinel.utxo")
     }
 
-    fun getCollectionStore(): Book? {
-        return Paper.bookOn(storeLocation, "sentinel.collections")
+    fun getCollectionStore(): PayloadRecord {
+        return PayloadRecord(storeLocation, "collections.payload")
+    }
+
+
+    fun getDojoStore(): PayloadRecord {
+        return PayloadRecord(storeLocation, "dojo.payload")
     }
 }
