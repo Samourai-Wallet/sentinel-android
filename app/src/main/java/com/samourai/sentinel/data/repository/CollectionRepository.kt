5e18d051a64ee5ba3a6b4308b8bf12cce15c25ce
Sarath <sarathkumars677@gmail.com>
2020-08-31 17:01:53 +0530

    Updated collection repository

--- app/src/main/java/com/samourai/sentinel/data/repository/CollectionRepository.kt
+++ app/src/main/java/com/samourai/sentinel/data/repository/CollectionRepository.kt
@@ -2,25 +2,29 @@ package com.samourai.sentinel.data.repository
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.samourai.sentinel.core.access.AccessFactory
 import com.samourai.sentinel.data.PubKeyCollection
 import com.samourai.sentinel.data.db.DbHandler
+import com.samourai.sentinel.service.WebSocketHandler
+import com.samourai.sentinel.util.apiScope
+import com.samourai.sentinel.util.dataBaseScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.java.KoinJavaComponent.inject
 import java.util.*
 import kotlin.collections.ArrayList
 
 class CollectionRepository {
 
-    val pubKeyCollections: ArrayList<PubKeyCollection> = arrayListOf();
+    val pubKeyCollections: ArrayList<PubKeyCollection> = arrayListOf()
     val collectionsLiveData: MutableLiveData<ArrayList<PubKeyCollection>> = MutableLiveData();
 
     private val dbHandler: DbHandler by inject(DbHandler::class.java)
 
     fun addNew(pubKeyCollection: PubKeyCollection) {
         pubKeyCollection.id = UUID.randomUUID().toString()
-        pubKeyCollections.add(pubKeyCollection);
+        pubKeyCollections.add(pubKeyCollection)
         this.sync()
     }
 
@@ -31,51 +35,76 @@ class CollectionRepository {
 
     fun update(pubKeyCollection: PubKeyCollection, index: Int) {
         pubKeyCollections[index] = pubKeyCollection
-        Log.i("collection:update", "${pubKeyCollections[index]}")
+        pubKeyCollections[index].updateBalance()
         this.sync()
     }
 
 
     fun findById(id: String): PubKeyCollection? {
-       return pubKeyCollections.find { it.id == id }
-    }
-
-    init {
-        this.read()
+        return pubKeyCollections.find { it.id == id }
     }
 
     /**
      * write changes to the db
      * sync needs to be called after changing collection (edit,delete,add)
      */
-    private fun sync() {
-        GlobalScope.launch(Dispatchers.IO) {
-            dbHandler.getCollectionStore()?.write("collections", pubKeyCollections);
+    @Synchronized
+    fun sync() {
+        val dupRemoved = pubKeyCollections.distinctBy { it.id }
+        pubKeyCollections.clear()
+        pubKeyCollections.addAll(dupRemoved)
+        dataBaseScope.launch(Dispatchers.IO) {
+            dbHandler.getCollectionStore().write(pubKeyCollections);
         }
         this.emit()
     }
 
-
-    fun read() = GlobalScope.launch(Dispatchers.IO) {
+    @Synchronized
+    fun read() = dataBaseScope.launch(Dispatchers.IO) {
+        pubKeyCollections.clear()
         try {
-            val readValue: ArrayList<PubKeyCollection> = dbHandler.getCollectionStore()?.read("collections")
-                    ?: arrayListOf();
+            val readValue: ArrayList<PubKeyCollection> = dbHandler.getCollectionStore().read()
+                    ?: arrayListOf()
             if (readValue.size != 0) {
-                pubKeyCollections.clear()
-                pubKeyCollections.addAll(readValue);
+                pubKeyCollections.addAll(readValue.distinctBy { it.id })
             } else {
-                pubKeyCollections.clear();
+                pubKeyCollections.clear()
             }
         } catch (e: Exception) {
             pubKeyCollections.clear()
-            throw  e;
+            throw  e
         }
-        collectionsLiveData.postValue(pubKeyCollections);
+        emit()
     }
 
 
     private fun emit() {
-        collectionsLiveData.postValue(pubKeyCollections);
+        val array = pubKeyCollections.distinctBy { it.id }
+        collectionsLiveData.postValue(array as ArrayList<PubKeyCollection>)
+//        if (array.isNotEmpty())
+//            apiScope.launch(Dispatchers.Main) {
+//
+//            }
+    }
+
+    fun update(pubKeyCollection: PubKeyCollection) {
+        if (this.pubKeyCollections.contains(pubKeyCollection)) {
+            return this.update(pubKeyCollection, this.pubKeyCollections.indexOf(pubKeyCollection))
+        }
+    }
+
+    suspend fun remove(id: String) = withContext(Dispatchers.IO) {
+        val item = pubKeyCollections.find { it.id == id }
+        pubKeyCollections.remove(item)
+        sync()
+        withContext(Dispatchers.Main) {
+            collectionsLiveData.postValue(pubKeyCollections)
+        }
+    }
+
+    fun reset() {
+        this.pubKeyCollections.clear()
+        this.sync()
     }
 
 }
