ba07f74b58ec14c78d6476859b3dc68b2839e629
Rocky Rococo <sam@samourai.io>
2020-09-01 16:24:07 +0000

    Add HomeActivityTest.kt kotlin test class

---
+++ app/src/test/java/com/samourai/sentinel/ui/home/HomeActivityTest.kt
@@ -0,0 +1,40 @@
+package com.samourai.sentinel.ui.home
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class HomeActivityTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onResume() {
+    }
+
+    @Test
+    fun onRequestPermissionsResult() {
+    }
+
+    @Test
+    fun onOptionsItemSelected() {
+    }
+
+    @Test
+    fun onCreateOptionsMenu() {
+    }
+
+    @Test
+    fun onBackPressed() {
+    }
+
+    @Test
+    fun onPrepareOptionsMenu() {
+    }
+
+    @Test
+    fun connectSocket() {
+    }
+}
