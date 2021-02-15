0602c6172d383bb3bf633f132b49263948ce6b87
Sarath <sarathkumars677@gmail.com>
2021-02-15 23:36:45 +0530

    get tx by pubkey

--- app/src/main/java/com/samourai/sentinel/data/db/dao/TxDao.kt
+++ app/src/main/java/com/samourai/sentinel/data/db/dao/TxDao.kt
@@ -25,6 +25,9 @@ interface TxDao {
     @Query("SELECT * from transactions WHERE collectionId=:collectionID AND associatedPubKey=:pubKey  ORDER BY time DESC LIMIT 70")
     fun getAssociated(collectionID: String, pubKey: String): List<Tx>
 
+    @Query("SELECT * from transactions WHERE associatedPubKey=:pubKey  ORDER BY time DESC LIMIT 70")
+    fun getAssociated(pubKey: String): List<Tx>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(tx: Tx)
 
